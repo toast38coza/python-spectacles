fe63c2ddfb0771bca406ba3810ad4ca06d561323
toast38coza <info@38.co.za>
2017-07-12 12:24:33 +0200

    some issues with getting requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='python-spectacles',
-      version='1.4.1',
+      version='1.4.2',
       description='Description',
       author=u'Christo Crampton',
       license='MIT',
       packages=['spectacles'],
-      install_requires=required,
+      install_requires=[
+          'splinter==0.7.5',
+          'requests==2.10.0',
+          'PyYAML==3.11',
+          'click==6.6'
+      ],
       zip_safe=False)
