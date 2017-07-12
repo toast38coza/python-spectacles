f63a76d6fda59a58a19defbdca9bf3271b20f6bb
toast38coza <info@38.co.za>
2017-07-12 12:20:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
-# -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(
-    name='python-spectacles',
-    version='1.4.1',
-    author=u'Christo Crampton',
-    packages=['spectacles'],
-    include_package_data=True,
-    install_requires=required,
-    entry_points='''
-        [console_scripts]
-        spectacles=spectacles.runner:run
-    ''',
-    url='https://github.com/toast38coza/python-spectacles',
-    license='MIT licence, see LICENCE',
-    description='Write e2e tests in yml. Run with Selenium. Report with Markdown',
-    long_description=open('README.md').read(),
-)
+setup(name='python-spectacles',
+      version='1.4.1',
+      description='Description',
+      author=u'Christo Crampton',
+      license='MIT',
+      packages=['spectacles'],
+      install_requires=required,
+      zip_safe=False)
